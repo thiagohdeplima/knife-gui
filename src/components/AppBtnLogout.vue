74144bf8e7bad58b5cf40de1a8d3011abf448135
Thiago Henrique <fale@thiago.tech>
2022-04-23 14:13:59 -0300

    chore: create button components

---
+++ src/components/AppBtnLogout.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn icon>
+    <v-icon>mdi-logout</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'AppBtnLogout',
+}
+</script>
---
+++ src/components/AppBtnNotification.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn icon class="pa-r12">
+    <v-icon v-if="active">{{ getIcon() }}</v-icon>
+  </v-btn>
+</template>
+
+
+<script>
+export default {
+  name: 'AppBtnNotification',
+
+  data: () => ({
+    active: true
+  }),
+
+  methods: {
+    getIcon: function() {
+      let icon = "mdi-bell-outline"
+
+      if(this.hasNotifications()) {
+        icon = "mdi-bell-ring"
+      }
+
+      return icon
+    },
+
+    hasNotifications: function() {
+      return [true, false][(Math.random() * 2) | 0]
+    }
+  }
+}
+</script>
